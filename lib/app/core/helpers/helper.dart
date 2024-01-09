7dd6a2c3f9f676d30ec4c1408412d39531012612
Felipe Costa <fcostax02@gmail.com>
2024-01-09 10:10:18 -0300

    flavors on Android

---
+++ lib/app/core/helpers/helper.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/services.dart';
+
+const bool isSunflowrInDebug = appFlavor == null || appFlavor == 'dev';
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sunflowr/app/core/helpers/helper.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,10 +9,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: isSunflowrInDebug,
       title: 'Flutter App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFAECCAB)),
